cba2f2540c3cf851e2e87591edaa856e7f6052c3
Gabe Imlay <gabe.imlay9271@gmail.com>
2024-02-08 14:17:10 -0500

    Added some fixes for the nav bar

--- scripts/active-link-navbar.js
+++ scripts/active-link-navbar.js
@@ -4,15 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
 
     navLinks.forEach(link => {
-        const linkHref = link.getAttribute("href");
+        const linkHref = link.href; // Get the full URL of the link
 
         if (currentLocation === linkHref) {
             // Remove the active class from all links before adding it to the current one
             navLinks.forEach(navLink => {
                 navLink.classList.remove("active");
+                navLink.classList.remove("--bs-orange"); // Remove the custom orange class from all links
             });
 
             link.classList.add("active");
+            link.classList.add("--bs-orange"); // Add the custom orange class to the current link
         }
     });
 });
+
